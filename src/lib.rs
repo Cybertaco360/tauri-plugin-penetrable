93991a411535e8c6091860fa50bec720854607e4
Goldy Yan <151985789+Cybertaco360@users.noreply.github.com>
2025-05-13 21:51:51 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,19 @@ mod com;
 
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("penetrable")
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![enable_penetrable, disable_penetrable])
         .on_window_ready(|window| {
-            com::windows::penetrable(window);
+            // Optional: Enable penetrable by default or leave it as an explicit action.
         })
         .build()
 }
 
+#[tauri::command]
+fn enable_penetrable<R: Runtime>(window: Window<R>) {
+    com::windows::penetrable(window);
+}
+
+#[tauri::command]
+fn disable_penetrable<R: Runtime>(window: Window<R>) {
+    com::windows::disable_penetrable(window);
+}
